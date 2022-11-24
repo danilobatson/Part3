9cc6aea979019adc2318038ffca5769592c6dcec
danilobatson <djbatson19@gmail.com>
2022-11-24 12:20:38 -0800

    move to async

--- phonebook/app.js
+++ phonebook/app.js
@@ -1,11 +1,18 @@
 const config = require('./utils/config');
 const express = require('express');
 const app = express();
+
 const cors = require('cors');
 const personsRouter = require('./controllers/persons');
-const middleware = require('./utils/middleware');
+
 const { info, error } = require('./utils/logger');
+const {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+} = require('./utils/middleware');
 const mongoose = require('mongoose');
+
 
 info('connecting to', config.MONGODB_URI);
 
@@ -21,11 +28,11 @@ mongoose
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
-app.use(middleware.requestLogger);
+app.use(requestLogger);
 
 app.use('/api/notes', personsRouter);
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 module.exports = app;
--- phonebook/controllers/persons.js
+++ phonebook/controllers/persons.js
@@ -1,84 +1,68 @@
 const personsRouter = require('express').Router();
 const Person = require('../models/persons');
 
-personsRouter.get('/', (req, res) => {
-  Person.find({}).then((persons) => {
-    res.json(persons);
-  });
+personsRouter.get('/', async (req, res) => {
+  const persons = await Person.find({});
+  res.json(persons);
 });
 
-personsRouter.get('/:id', (req, res, next) => {
-  Person.findById(req.params.id)
-    .then((person) => {
+personsRouter.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const person = await Person.findById(id);
+    if (person) {
       res.json(person);
-    })
-    .catch((error) => next(error));
+    } else {
+      res.status(404).end();
+    }
+  } catch (exception) {
+    next(exception);
+  }
 });
 
-personsRouter.post('/', (req, res, next) => {
+personsRouter.post('/', async (req, res, next) => {
   const { name, number } = req.body;
-  const body = req.body;
 
   const person = new Person({
     name,
     number,
   });
-  person
-    .save()
-    .then((person) => {
-      res.json(person);
-    })
-    .catch((error) => next(error));
 
-  // !name
-  //   ? res.status(400).json({ error: 'name missing' })
-  //   : !number
-  //   ? res.status(400).json({ error: 'number missing' })
-  //   : !body
-  //   ? res.status(400).json({ error: 'body missing' })
-  //   : name.length < 3
-  //   ? res.status(400).json({ error: 'name must be at least 3 characters long' })
-  //   : null;
-
-  // Person.findOneAndUpdate({ name: body.name }, { number: body.number }).then(
-  //   (persons) => {
-  //     if (persons) {
-  //       res.json(persons);
-  //     } else {
-  //       const person = new Person({
-  //         name: body.name,
-  //         number: body.number,
-  //       });
-  //       person
-  //         .save()
-  //         .then((savedPerson) => {
-  //           res.json(savedPerson);
-  //         })
-  //         .catch((error) => next(error));
-  //     }
-  //   }
-  // );
+  try {
+    const savedPerson = await person.save();
+    res.json(savedPerson);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
-personsRouter.delete(':id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch((error) => next(error));
+personsRouter.delete(':id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await Person.findByIdAndDelete(id);
+    res.status(204).end();
+  } catch (exception) {
+    next(exception);
+  }
 });
 
-personsRouter.put('/:id', (req, res, next) => {
+personsRouter.put('/:id', async (req, res, next) => {
   const { name, number } = req.body;
-  Person.findByIdAndUpdate(
-    req.params.id,
-    { name, number },
-    { new: true, runValidators: true, context: 'query' }
-  )
-    .then((updatedPerson) => {
-      res.json(updatedPerson);
-    })
-    .catch((error) => next(error));
+  const person = {
+    name,
+    number,
+  };
+
+  try {
+    const updatedPerson = await Person.findByIdAndUpdate(
+      req.params.id,
+      person,
+      { new: true, runValidators: true, context: 'query' }
+    );
+    res.json(updatedPerson);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = personsRouter;
